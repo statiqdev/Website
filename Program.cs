a486af53b4b13bb5e5576ee113b6e35d05234ea6
Dave Glick <dave@daveaglick.com>
2020-03-09 23:00:45 -0400

    Initial Statiq site layout

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Statiq.App;
+using Statiq.Web;
+
+namespace Statiq.Dev
+{
+    public static class Program
+    {
+        public static async Task<int> Main(string[] args) =>
+            await Bootstrapper.Factory
+                .CreateWeb(args)
+                .RunAsync();
+    }
+}
