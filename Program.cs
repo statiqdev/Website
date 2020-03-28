55173bbd8e2de47432ec1ca784c1486e1b57f1bd
Dave Glick <dave@daveaglick.com>
2020-03-27 23:51:58 -0400

    More work on blog

--- Program.cs
+++ Program.cs
@@ -12,6 +12,14 @@ namespace Statiq.Dev
         public static async Task<int> Main(string[] args) =>
             await Bootstrapper.Factory
                 .CreateWeb(args)
+                .AddSetting(Keys.Host, "statiq.dev")
+                .AddSetting(Keys.LinksUseHttps, true)
+                .AddSetting(
+                    Keys.DestinationPath,
+                    Config.FromDocument(doc => doc.Source.Parent.Segments.Last().SequenceEqual("posts".AsMemory())
+                        ? new NormalizedPath("blog").Combine(doc.GetDateTime(WebKeys.Published).ToString("yyyy/MM/dd")).Combine(doc.Destination.FileName.ChangeExtension(".html"))
+                        : doc.Destination.ChangeExtension(".html")))
+                .AddSetting("EditLink", Config.FromDocument((doc, ctx) => new NormalizedPath("https://github.com/statiqdev/statiq.dev/edit/master/input").Combine(doc.Source.GetRelativeInputPath(ctx))))
                 .RunAsync();
     }
 }
--- input/blog/posts/new-statiq-website.md
+++ input/blog/posts/new-statiq-website.md
@@ -1,7 +1,6 @@
 ﻿Title: New Statiq Website
 Published: 3/22/2020
-Tags:
-  - Meta
+Topic: meta
 ---
 I know, I know. It's a little embarrassing that a static site generator didn't actually have a static site (or any site for that matter). In my defense, I've been working so hard on actually building the thing that getting a site up and running was a secondary concern. Well the embarrassment is over and Statiq now has a lovely information and documentation site (if I do say so myself).
 
