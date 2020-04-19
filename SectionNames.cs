e1305753a7ad3c52aa11dbd2e82ddbbf31e8741b
Dave Glick <dave@daveaglick.com>
2020-04-19 12:39:34 -0400

    Rewrote license file to use Markdown shortcode

--- SectionNames.cs
+++ SectionNames.cs
@@ -7,6 +7,7 @@ namespace Statiqdev
     public static class SectionNames
     {
         public const string Head = nameof(Head);
+        public const string Scripts = nameof(Scripts);
         public const string Splash = nameof(Splash);
         public const string Sidebar = nameof(Sidebar);
         public const string Subtitle = nameof(Subtitle);
