314ac7e061ded7b36842b8a89ade613b350474a3
Dave Glick <dave@daveaglick.com>
2021-09-27 11:00:39 -0400

    Refactoring for use with a Statiq Docs theme

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Statiq.App;
 using Statiq.Common;
 using Statiq.Web;
+using Statiq.Docs;
 
 namespace Statiqdev
 {
@@ -10,7 +11,7 @@ namespace Statiqdev
     {
         public static async Task<int> Main(string[] args) =>
             await Bootstrapper.Factory
-                .CreateWeb(args)
+                .CreateDocs(args)
                 .AddMappedInputPath("external/Statiq.Web/examples/Statiq.Web.Examples/input", "web/examples")
                 .AddMappedInputPath("external/Statiq.Web/examples/Statiq.Web.Examples/input/Shared", "Shared") // Promote the partial views to the root Shared folder
                 .RunAsync();
